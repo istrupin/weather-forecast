882e08c42739cd1269558fb0ea5b2527c50d3d32
Strupinskiy <IStrupin@russellinvestments.com>
2017-06-26 08:31:03 -0700

    remove apikey from file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '122b010c7ef75ae1332f4d5b55520edb';
+const API_KEY = 'nope';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
