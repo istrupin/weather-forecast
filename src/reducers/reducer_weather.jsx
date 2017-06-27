fe88bf3648961e894655133a51f7fcb43d83dd30
Strupinskiy <IStrupin@russellinvestments.com>
2017-06-27 16:03:16 -0700

    added to weather list

--- src/reducers/reducer_weather.jsx
+++ src/reducers/reducer_weather.jsx
@@ -1,6 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 
 export default function (state = [], action) {
-    switch(actop.type) {
+    switch(action.type) {
+    case FETCH_WEATHER:
+        return [action.payload.data, ...state];
     }
     console.log('Action received: ', action);
     return state;
