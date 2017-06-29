d0b5f85edff9156a4c533e9bc7a52dae814d4d5e
istrupin <istrupinskiy@russellinvestments.com>
2017-06-28 23:14:23 -0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '122b010c7ef75ae1332f4d5b55520edb';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
